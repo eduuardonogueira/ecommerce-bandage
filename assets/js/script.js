6885d85012eee8b392482bec5d13eb5c50726b0a
EduardoNogueiradev <eduardocastroemp@gmail.com>
2024-02-28 12:42:21 -0300

    added menu and js scripts

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById("menu-button");
+const icon = document.getElementById("menu-icon");
+const openIcon = document.getElementById("menu-open-icon");
+const menuLinks = document.getElementById("menu-links");
+
+menu.addEventListener("click", () => {
+  openIcon.classList.toggle("show");
+  icon.classList.toggle("hidden");
+  menuLinks.classList.toggle("show");
+});
+
+const menuShop = document.getElementById("menu-shop");
+const shopIcon = document.getElementById("shop-icon");
+const shopIconOpen = document.getElementById("shop-icon-open");
+const menuShopLinks = document.getElementById("menu-shop-links");
+
+menuShop.addEventListener("click", () => {
+  shopIconOpen.classList.toggle("show");
+  shopIcon.classList.toggle("hidden");
+  menuShopLinks.classList.toggle("show");
+});
