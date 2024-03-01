86f93f89852baa4cf4c0ddb5e76ac14943ae7fd8
EduardoNogueiradev <eduardocastroemp@gmail.com>
2024-02-29 23:21:08 -0300

    feat: Readme and new function in the javascript

---
+++ README.md
@@ -0,0 +1,35 @@
+# E-commerce Website - Bandage
+
+This repository contains the code for an e-commerce website developed as part of the UOL Compass project. The website is designed to showcase a variety of bandage products and provide users with a seamless shopping experience.
+
+## Figma Design
+
+The website design was created using Figma. You can view the design and prototype by following this link: [Figma - E-commerce UI Kit](https://www.figma.com/file/409yX6R4voVXSjNV8c4Od6/E-commerce-UI---Figma-Ecommerce-UI-Kit-(Demo-Version)-(Community)?type=design&node-id=2984-6636&mode=design&t=kjTbQ23DFhzLF8LV-0)
+
+## Live Website
+
+The website is deployed and accessible online. You can visit the live website by following this link: [E-commerce Bandage Website](https://eduuardonogueira.github.io/ecommerce-bandage/)
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Features
+
+- Responsive design for optimal viewing on various devices.
+- Intuitive navigation for easy browsing of products.
+- Product pages with detailed descriptions and images.
+- Seamless checkout process for a hassle-free shopping experience.
+
+
+## Issues and Feedback
+
+If you encounter any issues while using the website or have any feedback to share, please open an issue on this repository. Your input is valuable and helps us improve the website for all users.
+
+## Contact
+
+If you have any questions or need further assistance, you can reach out to the project team at [eduardocastroemp@gmail.com](eduardocastroemp@gmail.com).
+
+Thank you for your interest in our e-commerce website! We hope you enjoy browsing our selection of bandage products.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-const menu = document.getElementById("menu-button");
-const icon = document.getElementById("menu-icon");
-const openIcon = document.getElementById("menu-open-icon");
-const menuLinks = document.getElementById("menu-links");
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu-button");
+  const icon = document.getElementById("menu-icon");
+  const openIcon = document.getElementById("menu-open-icon");
+  const menuLinks = document.getElementById("menu-links");
 
-menu.addEventListener("click", () => {
-  openIcon.classList.toggle("show");
-  icon.classList.toggle("hidden");
-  menuLinks.classList.toggle("show");
-});
+  menu.addEventListener("click", () => {
+    openIcon.classList.toggle("show");
+    icon.classList.toggle("hidden");
+    menuLinks.classList.toggle("show");
+  });
 
-const menuShop = document.getElementById("menu-shop");
-const shopIcon = document.getElementById("shop-icon");
-const shopIconOpen = document.getElementById("shop-icon-open");
-const menuShopLinks = document.getElementById("menu-shop-links");
+  const menuShop = document.getElementById("menu-shop");
+  const shopIcon = document.getElementById("shop-icon");
+  const shopIconOpen = document.getElementById("shop-icon-open");
+  const menuShopLinks = document.getElementById("menu-shop-links");
 
-menuShop.addEventListener("click", () => {
-  shopIconOpen.classList.toggle("show");
-  shopIcon.classList.toggle("hidden");
-  menuShopLinks.classList.toggle("show");
+  menuShop.addEventListener("click", () => {
+    shopIconOpen.classList.toggle("show");
+    shopIcon.classList.toggle("hidden");
+    menuShopLinks.classList.toggle("show");
+  });
 });
